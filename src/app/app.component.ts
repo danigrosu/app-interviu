d91cdccb424550113d8b0c2a028890e361ae8611
raduMMR <westsergent@gmail.com>
2019-03-09 21:38:15 +0200

    feat: activare/dezactivare butoane in functie de ruta aleasa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  activateA = false;
+  activateB = false;
+  
+  constructor( private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((val) => {
+      if(val instanceof NavigationEnd) {
+        this.activateA = false;
+        this.activateB = false;
+
+        if(val.url.indexOf('/a') != -1) {
+          this.activateA = true;
+        }
+        if(val.url.indexOf('/b') != -1) {
+          this.activateB = true;
+        }
+
+      }
+    });
+  }
+
 }
